3755a2a7386f4a85015ab2d2395878b613f659d0
wleite <wleite@student.42sp.org.br>
2023-02-06 17:53:57 -0300

    feat: add container box style

--- awesome_app/lib/main.dart
+++ awesome_app/lib/main.dart
@@ -16,8 +16,25 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Awesome App"),
       ),
-      body: Container(
-        child: const Text("Hi Flutter"),
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.all(8),
+          width: 100,
+          height: 100,
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+              color: Colors.red,
+              borderRadius: BorderRadius.circular(10),
+              gradient:
+                  const LinearGradient(colors: [Colors.pink, Colors.yellow]),
+              boxShadow: const [BoxShadow(color: Colors.grey, blurRadius: 10)]),
+          child: const Text(
+            "I am a Box",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+                fontSize: 20, color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+        ),
       ),
     );
   }
