03939bd7f00ea3e066994b559a3dd9458a5f3d60
wleite <wleite@student.42sp.org.br>
2023-02-08 16:02:30 -0300

    feat: add input name TextField

--- awesome_app/lib/main.dart
+++ awesome_app/lib/main.dart
@@ -13,31 +13,60 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[200],
       appBar: AppBar(
         title: const Text("Awesome App"),
       ),
       body: Center(
-        child: Container(
-          height: 100,
-          width: 100,
-          color: Colors.teal,
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: SingleChildScrollView(
+            child: Card(
+              child: Column(children: [
+                Image.asset("assets/bg.jpeg"),
+                SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  "Change my name",
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: TextField(
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: "Enter Some Text",
+                        labelText: "Name"),
+                  ),
+                )
+              ]),
+            ),
+          ),
         ),
       ),
       drawer: Drawer(
         child: ListView(
           padding: const EdgeInsets.all(0),
-          children: const [
-            DrawerHeader(
-              child: Text("Hi I am a drawer."),
-              decoration: BoxDecoration(color: Colors.purple),
+          children: [
+            const UserAccountsDrawerHeader(
+              accountName: Text("Welton Leite"),
+              accountEmail: Text("example@test.com"),
+              currentAccountPicture: CircleAvatar(
+                  backgroundImage: NetworkImage(
+                      "https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80")),
             ),
             ListTile(
               leading: Icon(Icons.person),
               title: Text("Welton Leite"),
               subtitle: Text("Human Coder"),
               trailing: Icon(Icons.edit),
+              onTap: () {},
             ),
-            ListTile(
+            const ListTile(
               leading: Icon(Icons.person),
               title: Text("Email"),
               subtitle: Text("example@test.com"),
@@ -48,7 +77,7 @@ class HomePage extends StatelessWidget {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
-        child: Icon(Icons.edit),
+        child: Icon(Icons.send),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
     );
